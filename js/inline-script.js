dee77877bd35d4c5f444640b48ce157a3ce20636
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2022-09-08 13:32:05 +0200

    Run everything after window load

--- js/inline-script.js
+++ js/inline-script.js
@@ -9,76 +9,76 @@ jQuery( function( $ ) {
 		}
 	);
 
-	$( '.comment-form' ).each( function() {
-		var $this  = $( this ),
-			$input    = $this.find( 'input' ),
-			$select   = $this.find( 'select' ),
-			$textarea = $this.find( 'textarea' );
+	$window.load( function () {
+		$( '.comment-form' ).each( function() {
+			var $this  = $( this ),
+				$input    = $this.find( 'input' ),
+				$select   = $this.find( 'select' ),
+				$textarea = $this.find( 'textarea' );
 
-		$input.each( function() {
-			var $this     = $( this ),
-				type      = $this.attr( 'type' ),
-				name      = $this.attr( 'name' ),
-				url_value = getUrlParam( name ),
-				value;
+			$input.each( function() {
+				var $this     = $( this ),
+					type      = $this.attr( 'type' ),
+					name      = $this.attr( 'name' ),
+					url_value = getUrlParam( name ),
+					value;
 
-			// No url value found.
-			if ( null === url_value ) {
-				return true;
-			}
+				// No url value found.
+				if ( null === url_value ) {
+					return true;
+				}
 
-			switch ( type ) {
-				case 'radio':
-					value = $this.attr( 'value' );
-					$this.prop( 'checked', ( value === url_value ) );
-					break;
-				case 'checkbox':
-					$this.prop( 'checked', Boolean( url_value ) );
-					break;
-				default:
+				switch ( type ) {
+					case 'radio':
+						value = $this.attr( 'value' );
+						$this.prop( 'checked', ( value === url_value ) );
+						break;
+					case 'checkbox':
+						$this.prop( 'checked', Boolean( url_value ) );
+						break;
+					default:
+						value = $this.val();
+						if ( ! value ) {
+							$this.val( url_value );
+						}
+						break;
+				}
+				_debug( 'input ' + type + ': ' + name + ' : ' + value );
+			} );
+
+			$select.each( function () {
+				var $this = $( this ),
+					name  = $this.attr( 'name' ),
 					value = $this.val();
-					if ( ! value ) {
-						$this.val( url_value );
+				if ( ! value ) {
+					value = getUrlParam( name );
+					if ( value ) {
+						$this.val( value );
 					}
-					break;
-			}
-			_debug( 'input ' + type + ': ' + name + ' : ' + value );
-		} );
-
-		$select.each( function () {
-			var $this = $( this ),
-				name  = $this.attr( 'name' ),
-				value = $this.val();
-			if ( ! value ) {
-				value = getUrlParam( name );
-				if ( value ) {
-					$this.val( value );
 				}
-			}
-			_debug( 'select: ' + name + ' : ' + value );
-		} );
+				_debug( 'select: ' + name + ' : ' + value );
+			} );
 
-		$textarea.each( function () {
-			var $this = $( this ),
-				name  = $this.attr( 'name' ),
-				value = $this.val();
-			if ( ! value ) {
-				value = getUrlParam( name, value );
-				if ( value ) {
-					$this.val( value );
+			$textarea.each( function () {
+				var $this = $( this ),
+					name  = $this.attr( 'name' ),
+					value = $this.val();
+				if ( ! value ) {
+					value = getUrlParam( name, value );
+					if ( value ) {
+						$this.val( value );
+					}
 				}
-			}
-			_debug( 'textarea: ' + name + ' : ' + value );
-		} );
+				_debug( 'textarea: ' + name + ' : ' + value );
+			} );
 
-		var rating = $this.find( '#rating' );
-		if ( rating.length ) {
-			$window.load( function () {
+			var rating = $this.find( '#rating' );
+			if ( rating.length ) {
 				var value = rating.val();
 				$this.find( '.star-' + value ).click();
 				_debug( 'rating: ' + value );
-			} );
-		}
+			}
+		} );
 	} );
 
 	function getUrlParam( parameter, default_value ) {
