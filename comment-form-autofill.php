b18073abaee30303a6eacc47c7710a51c0874536
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2022-09-12 12:16:12 +0200

    v1.2

--- comment-form-autofill.php
+++ comment-form-autofill.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Comment form autofill
  * Description:       Autofill comment form fields by URL parameters.
- * Version:           1.1
+ * Version:           1.2
  * Author:            Jory Hogeveen
  * Author URI:        https://www.keraweb.nl
  * Text Domain:       comment-form-autofill
